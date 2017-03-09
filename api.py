911c464ec6c6694ee1c7dba15d227f10932e60b9
Wambui Sharon <wambuisha50@gmail.com>
2017-03-09 19:45:00 +0300

    Add the updated console app

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 print "Welcome to our cool weather console app" 
@@ -7,8 +8,10 @@ city = raw_input("Enter city name: ")
 
 api_key = "077936f695f61908cd19a5a2452a97fb"
 
-response = requests.get("http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}".format(city, api_key))
+response = requests.get("http://api.openweathermap.org/data/2.5/weather?q={0}&appid=077936f695f61908cd19a5a2452a97fb".format(city, api_key))
 
-print response.json()
+weather = response.json()
+
+print "The current weather in {0} is {1}".format(city, weather["weather"][0]["description"])
 
 
